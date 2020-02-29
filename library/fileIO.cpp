35a56332bbbba9f9a10d4329925991dcefd3cfef
MattGabris <matthew.gabris.17@cnu.edu>
2020-02-29 15:32:00 -0500

    Implemented loadPatrons

--- library/fileIO.cpp
+++ library/fileIO.cpp
@@ -53,16 +53,16 @@ int loadBooks(std::vector<book> &books, const char* filename) // from File to Ve
 
 		// CHECK : does the file have any data? TODO Might not be the right place to put this
 		if (curbook == ""){
-			return NO_BOOKS_IN_LIBRARY;
+			return NO_BOOKS_IN_LIBRARY; // RETURN because there was no data in the file
 		}
 
-		// Take data from input and stores data
+		// Take data from input and store data
 		std::getline(ss, curbookid, CHAR_TO_SEARCH_FOR); // reads line until delim, gets curbookid
 		std::getline(ss, curbooktitle, CHAR_TO_SEARCH_FOR); // reads line until delim, gets curbooktitle
 		std::getline(ss, curbookauthor, CHAR_TO_SEARCH_FOR); // reads line until delim, gets curbookauthor
 		std::getline(ss, curbookstate, CHAR_TO_SEARCH_FOR); // reads line until delim, gets curbookstate
 		std::getline(ss, curbookpatron, CHAR_TO_SEARCH_FOR); // reads line until delim, gets curbookpatron
-
+		// CLEARS the input vector
 		// Convert data as necessary and push into tempbook struct
 		tempbook.book_id = stoi(curbookid);
 		tempbook.title = curbooktitle;
@@ -87,7 +87,7 @@ int loadBooks(std::vector<book> &books, const char* filename) // from File to Ve
 	// CLOSE file
 	inStreamBook.close();
 
-	// CHECK : does the books vector have any data?
+	// CHECK : does the books vector have any data? (Check number 2)
 	if (books.size() == 0){	// if no books in library
 		return NO_BOOKS_IN_LIBRARY;	// RETURN because the input vector was empty
 	}
@@ -142,7 +142,65 @@ int saveBooks(std::vector<book> &books, const char* filename) // from Vector to 
  * */
 int loadPatrons(std::vector<patron> &patrons, const char* filename)
 {
-	return SUCCESS;
+	// CLEARS the input vector
+	patrons.clear();
+
+	// Create an input stream for the patrons from the input file and open it
+	ifstream inStreamPatron;
+	inStreamPatron.open(filename, ios::in);
+
+	// CHECK : is the file open?
+	if (!inStreamPatron.is_open()){
+		return COULD_NOT_OPEN_FILE;
+	}
+	// From here on out, assume the file is open
+
+	// Variable to hold parts parsed from the input file
+	std::string curpatron; // string to hold line of text from file containing all the patron's info
+	std::string curpatronid; // string to hold curpatron's ID
+	std::string curpatronname; // string to hold curpatron's title
+	std::string curpatronnumbooks; // string to hold curpatron's author
+
+	// Create temp patron object from struct to fill with info and push into vector patrons
+	patron temppatron;
+	std::stringstream ss;
+
+	// While the end of the file has not been reached
+	while (!inStreamPatron.eof()){
+		std::getline(inStreamPatron, curpatron); // load new line (each line = 1 patron's info)
+		ss.str(curpatron); // convert patron stream from input stream to string stream and store in ss
+
+		// CHECK : does the file have any data? TODO Might not be the right place to put this
+		if (curpatron == ""){
+			return NO_PATRONS_IN_LIBRARY; // RETURN because there was no data in the file
+		}
+
+		// Take data from input and store data
+		std::getline(ss, curpatronid, CHAR_TO_SEARCH_FOR); // reads line until delim, gets curpatronid
+		std::getline(ss, curpatronname, CHAR_TO_SEARCH_FOR); // reads line until delim, gets curpatronname
+		std::getline(ss, curpatronnumbooks, CHAR_TO_SEARCH_FOR); // reads line until delim, gets curpatronnumbooks
+
+		// Convert data as necessary and push into temppatron struct
+		temppatron.patron_id = stoi(curpatronid);
+		temppatron.name = curpatronname;
+		temppatron.number_books_checked_out = stoi(curpatronnumbooks);
+
+		// Push temporary patron onto the back of the patrons vector
+		patrons.push_back(temppatron);
+		// Clear the string stream to prepare for a new line of input
+		ss.clear();
+	}
+	// By this point all the data should have been parsed and organized
+
+	// CLOSE FILE
+	inStreamPatron.close();
+
+	// CHECK : does the books vector have any data? (Check number 2)
+	if (patrons.size() == 0){ // if no patrons in library
+		return NO_BOOKS_IN_LIBRARY; // RETURN because the input vector was empty
+	}
+
+	return SUCCESS; // RETURN because file was successfully iterated through and placed into a container
 }
 
 /* serializes patrons to the file filename
